cc2982c7a0a3d6a8136ef5cbf153c8f89a7643d6
Bojanacvetkovska <71123066+Bojanacvetkovska@users.noreply.github.com>
2022-09-07 16:29:40 +0200

    Initial commit

---
+++ src/main/java/at/bojana/probep/ProbepApplication.java
@@ -0,0 +1,13 @@
+package at.bojana.probep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProbepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/bojana/probep/ProbepApplicationTests.java
@@ -0,0 +1,13 @@
+package at.bojana.probep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProbepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
